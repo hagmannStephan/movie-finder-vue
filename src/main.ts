0663555d97ca9021ec445cdb4ed20a3942088b31
Hagmann Stephan <stephanhagmann@outlook.com>
2025-03-15 15:18:09 +0000

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import './assets/styles.scss'       // Global styles !Dont remove!
+
+createApp(App).mount('#app')
