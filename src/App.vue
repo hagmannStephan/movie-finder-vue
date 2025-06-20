f569024953ef14578f8dfd1c0825260b317eab69
esilva <esilva@ethz.ch>
2025-06-20 06:24:15 +0000

    Commiting newest changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import './assets/styles.scss'
+import './css/main.css';
+
+const loginRef = ref<Boolean>(false);
+const registerRef = ref<Boolean>(false);
+
+const login = () => {
+  loginRef.value = true;
+};
+
+const register = () => {
+  registerRef.value = true;
+};
 </script>
 
 <template>
@@ -7,67 +20,17 @@ import './assets/styles.scss'
     <div class="mainLogo">
       <img src="./assets/MovieFinderLogo.svg">
     </div>
+    <div class="screenPlaceholder">
+      <div v-if="loginRef === true" class="loginScreen">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+      </div>
+    </div>
     <div class="mainButtons">
-      <button class="mainButtonLogin">Login</button>
-      <button class="mainButtonRegister">Register</button>
+      <button @click="login" class="mainButtonLogin">Login</button>
+      <button @click="register" class="mainButtonRegister">Register</button>
     </div>
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-
-.main {
-  width: 16rem;
-}
-
-.mainLogo {
-  margin-top: 1.5rem;
-}
-
-.mainLogo img {
-  box-shadow: 4px 4px 16px var(--vermilion-light);
-}
-
-.mainButtons {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 1em;
-  margin-top: 14rem;
-}
-
-.mainButtons button {
-  height: 3rem;
-  font-size: 2rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  line-height: 1; /* Important for vertical centering */
-  padding: 0;     /* Remove any default padding */
-}
-
-
-.mainButtonLogin {
-  background-color: var(--raspberry);
-  box-shadow: 4px 4px 4px var(--raspberry-light);
-}
-
-.mainButtonRegister {
-  background-color: var(--vermilion);
-  box-shadow: 4px 4px 4px var(--vermilion-light);
-}
-</style>
+<style scoped></style>
